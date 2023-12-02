c54fe8ddc4ab002653dbd8170455a0d84a834091
erenroy <roypapan75@gmail.com>
2023-12-02 22:56:01 +0530

    add
    add

--- ytube/ytube/settings.py
+++ ytube/ytube/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ytube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['tplates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
